6930486e6645fc4ead1ea35398a29010c27c64af
Leo Li <acn_leoli@main.mtl.com.hk>
2018-09-02 00:12:16 +0800

    Init

--- Shutdown/MainForm.Designer.cs
+++ Shutdown/MainForm.Designer.cs
@@ -24,6 +24,8 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.tabCtrl = new System.Windows.Forms.TabControl();
 			this.tab1 = new System.Windows.Forms.TabPage();
@@ -31,31 +33,29 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.specifiedTime = new System.Windows.Forms.DateTimePicker();
 			this.tab2 = new System.Windows.Forms.TabPage();
-			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-			this.btnConfirm = new System.Windows.Forms.Button();
-			this.rbShutdown = new System.Windows.Forms.RadioButton();
-			this.rbHibernate = new System.Windows.Forms.RadioButton();
 			this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
-			this.label4 = new System.Windows.Forms.Label();
-			this.countDownMinute = new System.Windows.Forms.NumericUpDown();
-			this.label3 = new System.Windows.Forms.Label();
-			this.countDownHour = new System.Windows.Forms.NumericUpDown();
 			this.label2 = new System.Windows.Forms.Label();
-			this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
-			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.countDownHour = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.countDownMinute = new System.Windows.Forms.NumericUpDown();
+			this.label4 = new System.Windows.Forms.Label();
+			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.rbHibernate = new System.Windows.Forms.RadioButton();
+			this.rbShutdown = new System.Windows.Forms.RadioButton();
+			this.btnConfirm = new System.Windows.Forms.Button();
 			this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
 			this.toolStripContainer1.ContentPanel.SuspendLayout();
 			this.toolStripContainer1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.tabCtrl.SuspendLayout();
 			this.tab1.SuspendLayout();
 			this.flowLayoutPanel1.SuspendLayout();
 			this.tab2.SuspendLayout();
-			this.flowLayoutPanel2.SuspendLayout();
 			this.flowLayoutPanel3.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.countDownMinute)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.countDownHour)).BeginInit();
-			this.statusStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.countDownMinute)).BeginInit();
+			this.flowLayoutPanel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// toolStripContainer1
@@ -77,6 +77,24 @@
 			this.toolStripContainer1.Size = new System.Drawing.Size(394, 180);
 			this.toolStripContainer1.TabIndex = 0;
 			this.toolStripContainer1.Text = "toolStripContainer1";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslStatus});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(394, 22);
+			this.statusStrip1.SizingGrip = false;
+			this.statusStrip1.TabIndex = 0;
+			// 
+			// tsslStatus
+			// 
+			this.tsslStatus.ForeColor = System.Drawing.Color.Red;
+			this.tsslStatus.Name = "tsslStatus";
+			this.tsslStatus.Size = new System.Drawing.Size(0, 17);
 			// 
 			// tableLayoutPanel1
 			// 
@@ -115,7 +133,7 @@
 			this.tab1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.tab1.Size = new System.Drawing.Size(378, 52);
 			this.tab1.TabIndex = 0;
-			this.tab1.Text = "指定时间";
+			this.tab1.Text = "Specific Time";
 			this.tab1.UseVisualStyleBackColor = true;
 			// 
 			// flowLayoutPanel1
@@ -135,9 +153,9 @@
 			this.label1.Location = new System.Drawing.Point(4, 8);
 			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(73, 20);
+			this.label1.Size = new System.Drawing.Size(43, 20);
 			this.label1.TabIndex = 2;
-			this.label1.Text = "关机时间";
+			this.label1.Text = "Time";
 			// 
 			// specifiedTime
 			// 
@@ -145,7 +163,7 @@
 			this.specifiedTime.CustomFormat = "yyyy-MM-dd  HH:mm:ss";
 			this.specifiedTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
 			this.specifiedTime.ImeMode = System.Windows.Forms.ImeMode.Off;
-			this.specifiedTime.Location = new System.Drawing.Point(85, 5);
+			this.specifiedTime.Location = new System.Drawing.Point(55, 5);
 			this.specifiedTime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.specifiedTime.Name = "specifiedTime";
 			this.specifiedTime.ShowUpDown = true;
@@ -161,57 +179,8 @@
 			this.tab2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.tab2.Size = new System.Drawing.Size(378, 52);
 			this.tab2.TabIndex = 1;
-			this.tab2.Text = "倒计时";
+			this.tab2.Text = "Count Down";
 			this.tab2.UseVisualStyleBackColor = true;
-			// 
-			// flowLayoutPanel2
-			// 
-			this.flowLayoutPanel2.Controls.Add(this.rbHibernate);
-			this.flowLayoutPanel2.Controls.Add(this.rbShutdown);
-			this.flowLayoutPanel2.Controls.Add(this.btnConfirm);
-			this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 98);
-			this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-			this.flowLayoutPanel2.Size = new System.Drawing.Size(388, 33);
-			this.flowLayoutPanel2.TabIndex = 1;
-			// 
-			// btnConfirm
-			// 
-			this.btnConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnConfirm.Location = new System.Drawing.Point(152, 5);
-			this.btnConfirm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.btnConfirm.Name = "btnConfirm";
-			this.btnConfirm.Size = new System.Drawing.Size(58, 35);
-			this.btnConfirm.TabIndex = 5;
-			this.btnConfirm.Text = "确定";
-			this.btnConfirm.UseVisualStyleBackColor = true;
-			this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
-			// 
-			// rbShutdown
-			// 
-			this.rbShutdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.rbShutdown.AutoSize = true;
-			this.rbShutdown.Location = new System.Drawing.Point(78, 10);
-			this.rbShutdown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.rbShutdown.Name = "rbShutdown";
-			this.rbShutdown.Size = new System.Drawing.Size(66, 24);
-			this.rbShutdown.TabIndex = 4;
-			this.rbShutdown.Text = "关机";
-			this.rbShutdown.UseVisualStyleBackColor = true;
-			// 
-			// rbHibernate
-			// 
-			this.rbHibernate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.rbHibernate.AutoSize = true;
-			this.rbHibernate.Checked = true;
-			this.rbHibernate.Location = new System.Drawing.Point(4, 10);
-			this.rbHibernate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.rbHibernate.Name = "rbHibernate";
-			this.rbHibernate.Size = new System.Drawing.Size(66, 24);
-			this.rbHibernate.TabIndex = 3;
-			this.rbHibernate.TabStop = true;
-			this.rbHibernate.Text = "休眠";
-			this.rbHibernate.UseVisualStyleBackColor = true;
 			// 
 			// flowLayoutPanel3
 			// 
@@ -226,49 +195,22 @@
 			this.flowLayoutPanel3.Size = new System.Drawing.Size(370, 42);
 			this.flowLayoutPanel3.TabIndex = 0;
 			// 
-			// label4
+			// label2
 			// 
-			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(238, 8);
-			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(41, 20);
-			this.label4.TabIndex = 11;
-			this.label4.Text = "分钟";
-			// 
-			// countDownMinute
-			// 
-			this.countDownMinute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.countDownMinute.ImeMode = System.Windows.Forms.ImeMode.Off;
-			this.countDownMinute.Location = new System.Drawing.Point(170, 5);
-			this.countDownMinute.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.countDownMinute.Maximum = new decimal(new int[] {
-            999,
-            0,
-            0,
-            0});
-			this.countDownMinute.Name = "countDownMinute";
-			this.countDownMinute.Size = new System.Drawing.Size(60, 26);
-			this.countDownMinute.TabIndex = 9;
-			this.countDownMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-			// 
-			// label3
-			// 
-			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(121, 8);
-			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(41, 20);
-			this.label3.TabIndex = 12;
-			this.label3.Text = "小时";
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(4, 8);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(94, 20);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Count down";
 			// 
 			// countDownHour
 			// 
 			this.countDownHour.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.countDownHour.ImeMode = System.Windows.Forms.ImeMode.Off;
-			this.countDownHour.Location = new System.Drawing.Point(53, 5);
+			this.countDownHour.Location = new System.Drawing.Point(106, 5);
 			this.countDownHour.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.countDownHour.Maximum = new decimal(new int[] {
             999,
@@ -280,34 +222,92 @@
 			this.countDownHour.TabIndex = 10;
 			this.countDownHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			// 
-			// label2
+			// label3
 			// 
-			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(4, 8);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(41, 20);
-			this.label2.TabIndex = 8;
-			this.label2.Text = "倒数";
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(174, 8);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(52, 20);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "Hours";
 			// 
-			// tsslStatus
+			// countDownMinute
 			// 
-			this.tsslStatus.ForeColor = System.Drawing.Color.Red;
-			this.tsslStatus.Name = "tsslStatus";
-			this.tsslStatus.Size = new System.Drawing.Size(0, 17);
+			this.countDownMinute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.countDownMinute.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.countDownMinute.Location = new System.Drawing.Point(234, 5);
+			this.countDownMinute.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.countDownMinute.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+			this.countDownMinute.Name = "countDownMinute";
+			this.countDownMinute.Size = new System.Drawing.Size(60, 26);
+			this.countDownMinute.TabIndex = 9;
+			this.countDownMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			// 
-			// statusStrip1
+			// label4
 			// 
-			this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
-			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsslStatus});
-			this.statusStrip1.Location = new System.Drawing.Point(0, 0);
-			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(394, 22);
-			this.statusStrip1.SizingGrip = false;
-			this.statusStrip1.TabIndex = 0;
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(302, 8);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(42, 20);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Mins";
+			// 
+			// flowLayoutPanel2
+			// 
+			this.flowLayoutPanel2.Controls.Add(this.rbHibernate);
+			this.flowLayoutPanel2.Controls.Add(this.rbShutdown);
+			this.flowLayoutPanel2.Controls.Add(this.btnConfirm);
+			this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 98);
+			this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+			this.flowLayoutPanel2.Size = new System.Drawing.Size(388, 33);
+			this.flowLayoutPanel2.TabIndex = 1;
+			// 
+			// rbHibernate
+			// 
+			this.rbHibernate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.rbHibernate.AutoSize = true;
+			this.rbHibernate.Checked = true;
+			this.rbHibernate.Location = new System.Drawing.Point(4, 10);
+			this.rbHibernate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.rbHibernate.Name = "rbHibernate";
+			this.rbHibernate.Size = new System.Drawing.Size(104, 24);
+			this.rbHibernate.TabIndex = 3;
+			this.rbHibernate.TabStop = true;
+			this.rbHibernate.Text = "Hibernate";
+			this.rbHibernate.UseVisualStyleBackColor = true;
+			// 
+			// rbShutdown
+			// 
+			this.rbShutdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.rbShutdown.AutoSize = true;
+			this.rbShutdown.Location = new System.Drawing.Point(116, 10);
+			this.rbShutdown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.rbShutdown.Name = "rbShutdown";
+			this.rbShutdown.Size = new System.Drawing.Size(106, 24);
+			this.rbShutdown.TabIndex = 4;
+			this.rbShutdown.Text = "Shutdown";
+			this.rbShutdown.UseVisualStyleBackColor = true;
+			// 
+			// btnConfirm
+			// 
+			this.btnConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnConfirm.Location = new System.Drawing.Point(230, 5);
+			this.btnConfirm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnConfirm.Name = "btnConfirm";
+			this.btnConfirm.Size = new System.Drawing.Size(58, 35);
+			this.btnConfirm.TabIndex = 5;
+			this.btnConfirm.Text = "OK";
+			this.btnConfirm.UseVisualStyleBackColor = true;
+			this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
 			// 
 			// MainForm
 			// 
@@ -320,27 +320,27 @@
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "定时关机";
+			this.Text = "Shutdown";
 			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
 			this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
 			this.toolStripContainer1.ContentPanel.ResumeLayout(false);
 			this.toolStripContainer1.ResumeLayout(false);
 			this.toolStripContainer1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tabCtrl.ResumeLayout(false);
 			this.tab1.ResumeLayout(false);
 			this.flowLayoutPanel1.ResumeLayout(false);
 			this.flowLayoutPanel1.PerformLayout();
 			this.tab2.ResumeLayout(false);
-			this.flowLayoutPanel2.ResumeLayout(false);
-			this.flowLayoutPanel2.PerformLayout();
 			this.flowLayoutPanel3.ResumeLayout(false);
 			this.flowLayoutPanel3.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.countDownMinute)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.countDownHour)).EndInit();
-			this.statusStrip1.ResumeLayout(false);
-			this.statusStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.countDownMinute)).EndInit();
+			this.flowLayoutPanel2.ResumeLayout(false);
+			this.flowLayoutPanel2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
